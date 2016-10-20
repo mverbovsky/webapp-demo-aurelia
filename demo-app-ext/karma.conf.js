523a71c0ee71d37d7a83fbf79e7bc63f73c78135
Verbovský Martin <verbovsky@oksystem.local>
2016-10-20 17:29:34 +0200

    Implementace testu

--- demo-app-ext/karma.conf.js
+++ demo-app-ext/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
       typescript: require('typescript'),
       options: tsconfig.compilerOptions
     },
-    reporters: ['progress'],
+    reporters: ['progress', 'html'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- demo-app-ext/test/unit/app.spec.ts
+++ demo-app-ext/test/unit/app.spec.ts
@@ -1,7 +1,54 @@
 import {App} from '../../src/app';
 
-describe('the app', () => {
-  it('says hello', () => {
-    expect(new App().message).toBe('Hello World!');
+class RouterStub {
+  routes;
+  unknownRoute;
+  options = { pushState: false };
+  
+  configure(handler) {
+    handler(this);
+  }
+  
+  map(routes) {
+    this.routes = routes;
+  }
+
+  mapUnknownRoutes(route) {
+    this.unknownRoute = route;
+  }
+}
+
+describe('the App module', () => {
+  var sut, mockedRouter;
+
+  beforeEach(() => {
+    mockedRouter = new RouterStub();
+    sut = new App();
+    sut.configureRouter(mockedRouter, mockedRouter);
+  });
+
+  it('contains a router property', () => {
+    expect(sut.router).toBeDefined();
+  });
+
+  it('configures the router title', () => {
+    expect(sut.router.title).toEqual('Demo App');
+  });
+
+  it('configures the router pushState=true', () => {
+    expect(sut.router.options.pushState).toEqual(true);
+  });
+
+  it('should have a home route', () => {
+    expect(sut.router.routes).toContain({ route: ['','home'], moduleId: './routes/index', name: 'home' });
+  });
+
+  it('should have a person-table route', () => {
+     expect(sut.router.routes).toContain({ route: 'persons', moduleId: './routes/person-table', name: 'persons' });
+  });
+
+  it('should have a person-detail route', () => {
+    expect(sut.router.routes).toContain({ route: ['persons/:id','persons/new'],  moduleId: './routes/person-detail', name:'personDetail' });
   });
 });
+
